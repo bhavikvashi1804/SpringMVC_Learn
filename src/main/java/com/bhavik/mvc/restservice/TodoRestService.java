e28b9f53480515820f557005d8a6a92d776ba509
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-02-26 18:18:32 +0530

    Rest API: get with ID

--- src/main/java/com/bhavik/mvc/restservice/TodoRestService.java
+++ src/main/java/com/bhavik/mvc/restservice/TodoRestService.java
@@ -3,6 +3,8 @@ package com.bhavik.mvc.restservice;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,13 +14,25 @@ import com.bhavik.mvc.service.TodoService;
 
 @RestController
 public class TodoRestService {
-	
+
 	@Autowired
 	TodoService service;
-	
-	@RequestMapping(value = "/todos", method = RequestMethod.GET)
-	public List<Todo> getAllTodo(){
+
+	@GetMapping(value = "/todos")
+	public List<Todo> getAllTodo() {
 		return service.retrieveTodos("bhavik");
 	}
 
+	@GetMapping("/todos/{id}")
+	public Todo one(@PathVariable int id) {
+
+		Todo todo = new Todo();
+		List<Todo> list = service.retrieveTodos("bhavik");
+		for(Todo t:list) {
+			if(t.getId() == id) {
+				todo = t;
+			}
+		}
+		return todo;
+	}
 }
